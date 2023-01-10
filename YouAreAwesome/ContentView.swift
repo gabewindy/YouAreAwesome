c40359a7dacb1b1e7a1a92306a70b9fe47fee09b
gabriel <gabewindy@gmail.com>
2023-01-09 21:47:23 -0600

    Initial Commit

---
+++ YouAreAwesome/ContentView.swift
@@ -0,0 +1,40 @@
+//
+//  ContentView.swift
+//  YouAreAwesome
+//
+//  Created by Gab on 1/9/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var messageString = ""
+    var body: some View {
+        VStack {
+            Image(systemName: "swift")
+                .resizable()
+                .scaledToFit()
+                .foregroundColor(.orange) // comments
+                .background(.blue)
+                .padding(.trailing, 30.0)
+                .padding(.bottom, 100)
+            Text(messageString)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .foregroundColor(.red)
+                .italic()
+            HStack {
+                Button("awesome", action: {messageString = "you are awesome"}).buttonStyle(.borderedProminent) // standard format
+                Button("great") {
+                    messageString = "you are great"
+                }.buttonStyle(.borderedProminent) // trailing closure format
+            }
+        }.padding(.trailing, 100)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ YouAreAwesome/YouAreAwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesomeApp.swift
+//  YouAreAwesome
+//
+//  Created by Gab on 1/9/23.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
